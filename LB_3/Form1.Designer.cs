4f87876ca5ee00d0b73b2c9046aa5ea6ae789445
Курзенева Кристина <kristinakurzeneva35@gmail.com>
2024-12-12 09:17:28 +0500

    Добавление пакетов. Генерация контекста данных из существующей БД

--- LB_3/Form1.Designer.cs
+++ LB_3/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace LB_3
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            dataGridViewUsers = new DataGridView();
+            buttonSave = new Button();
+            panelTop = new Panel();
+            panelFill = new Panel();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewUsers).BeginInit();
+            panelTop.SuspendLayout();
+            panelFill.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dataGridViewUsers
+            // 
+            dataGridViewUsers.BackgroundColor = Color.White;
+            dataGridViewUsers.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewUsers.Dock = DockStyle.Fill;
+            dataGridViewUsers.Location = new Point(10, 10);
+            dataGridViewUsers.Margin = new Padding(4, 4, 4, 4);
+            dataGridViewUsers.Name = "dataGridViewUsers";
+            dataGridViewUsers.Size = new Size(1009, 532);
+            dataGridViewUsers.TabIndex = 0;
+            // 
+            // buttonSave
+            // 
+            buttonSave.Dock = DockStyle.Left;
+            buttonSave.Location = new Point(13, 14);
+            buttonSave.Margin = new Padding(4, 4, 4, 4);
+            buttonSave.Name = "buttonSave";
+            buttonSave.Size = new Size(96, 50);
+            buttonSave.TabIndex = 1;
+            buttonSave.Text = "Сохранить";
+            buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // panelTop
+            // 
+            panelTop.Controls.Add(buttonSave);
+            panelTop.Dock = DockStyle.Top;
+            panelTop.Location = new Point(0, 0);
+            panelTop.Margin = new Padding(4, 4, 4, 4);
+            panelTop.Name = "panelTop";
+            panelTop.Padding = new Padding(13, 14, 13, 14);
+            panelTop.Size = new Size(1029, 78);
+            panelTop.TabIndex = 2;
+            // 
+            // panelFill
+            // 
+            panelFill.Controls.Add(dataGridViewUsers);
+            panelFill.Dock = DockStyle.Fill;
+            panelFill.Location = new Point(0, 78);
+            panelFill.Margin = new Padding(4, 4, 4, 4);
+            panelFill.Name = "panelFill";
+            panelFill.Padding = new Padding(10);
+            panelFill.Size = new Size(1029, 552);
+            panelFill.TabIndex = 3;
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1029, 630);
+            Controls.Add(panelFill);
+            Controls.Add(panelTop);
+            Font = new Font("Segoe UI", 12F);
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "FormMain";
+            Text = "Пользователи";
+            ((System.ComponentModel.ISupportInitialize)dataGridViewUsers).EndInit();
+            panelTop.ResumeLayout(false);
+            panelFill.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private DataGridView dataGridViewUsers;
+        private Button buttonSave;
+        private Panel panelTop;
+        private Panel panelFill;
     }
 }
